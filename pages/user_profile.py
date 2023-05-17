65bce7cf140a287a778377b7cc574740be7a7332
kamalchibrani-ai <kchibrani@gmail.com>
2023-05-17 18:13:45 +0100

    made user_profile and pushed it to git

---
+++ pages/user_profile.py
@@ -0,0 +1,123 @@
+import streamlit as st
+from dotenv import load_dotenv
+import os
+import boto3
+from database import get_resourse,get_table
+load_dotenv('.env')
+from utils import switch_page_if_auth_isFalse,logout_button_sidebar,things_with_sidebar
+from decimal import Decimal
+
+switch_page_if_auth_isFalse()
+logout_button_sidebar()
+things_with_sidebar(st.session_state['username'])
+
+
+
+
+dynamodb = get_resourse()
+table = get_table('user_profile',dynamodb)
+
+print(table)
+
+# AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+# AWS_REGION = os.getenv('AWS_REGION')
+
+# dynamodb = boto3.resource('dynamodb', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_REGION)
+# table_name = 'user_profiles'
+# table = dynamodb.Table(table_name)
+
+def get_marks():
+    marks_10th = Decimal(str(st.number_input('Enter your 10th marks in percentage', min_value=0.0, max_value=100.0, step=0.1, key='marks_10th')))
+    marks_12th = Decimal(str(st.number_input('Enter your 12th marks in percentage', min_value=0.0, max_value=100.0, step=0.1, key='marks_12th')))
+    return marks_10th, marks_12th
+
+def get_degree_type():
+    degree_type = st.radio('Which degree are you interested in pursuing?', ('Bachelors', 'Masters'))
+    if degree_type == 'Masters':
+        bachelors = Decimal(str(st.number_input('Enter your Bachelors marks in CGPA', min_value=0.0, max_value=10.0, step=0.10, key='bachelors')))
+        return degree_type, bachelors
+    return degree_type, None
+
+def get_test_scores():
+    test_types = ('GRE', 'TOEFL', 'IELTS', 'GMAT', 'SAT', 'ACT', 'LSAT', 'MCAT', 'PCAT', 'DAT', 'OAT', 'Other')
+    test_type = st.multiselect('Which standardized tests have you taken or plan to take?', test_types, key='test_type')
+    test_scores = {}
+
+    if 'Other' in test_type:
+        other_test_name = st.text_input('Enter the name of the test').upper()
+        if len(other_test_name) < 0:
+            st.error('Please enter the name of the test before entering the score')
+    for t in test_type:
+        if t != 'Other':
+            test_scores[t] = Decimal(str(st.number_input(f'Enter your {t} score', min_value=0.0, max_value=500.0, step=0.1)))
+        else:
+            if len(other_test_name) > 0:
+                if other_test_name not in test_types:
+                    other_test_score = st.number_input(f'Enter your {other_test_name} score', min_value=0.0, max_value=500.0, step=0.1)
+                    test_scores[other_test_name] = Decimal(str(other_test_score))
+                    test_type.remove('Other')
+                else:
+                    st.error('Test already exists')
+            else:
+                st.error('Please enter the name of the test before entering the score')
+    return test_scores
+
+def get_country_and_field():
+    countries = ('USA', 'CANADA', 'UK', 'AUS', 'NEW ZEALAND', 'GERMANY', 'FRANCE', 'SINGAPORE', 'Other')
+    country = st.multiselect('Which country/countries do you want to study in?', countries, key='country')
+
+    if 'Other' in country:
+        other_country = st.text_input('Enter the name of the country').upper()
+        if len(other_country) < 0:
+            st.error('Please enter the name of the country before proceeding')
+        else:
+            if other_country in countries:
+                st.error('Country already exists')
+            else:
+                country.remove('Other')
+                country.append(other_country)
+
+    field = st.text_input('What field are you interested in studying?')
+    return country, field
+
+# Set page title
+# st.title('User Profile')
+
+# Get user input
+marks_10th, marks_12th = get_marks()
+degree_type, bachelors = get_degree_type()
+test_scores = get_test_scores()
+country, field = get_country_and_field()
+
+# Display user input
+# st.write('Your 10th marks:', marks_10th)
+# st.write('Your 12th marks:', marks_12th)
+# st.write('Degree type:', degree_type)
+# if bachelors:
+#     st.write('Your Bachelor marks:', bachelors)
+# for t, s in test_scores.items():
+#     st.write(f'{t} score:', s)
+# st.write('Desired country/countries:', country)
+# st.write('Field of study:', field)
+print(st.session_state['username'])
+
+# Save user input
+if st.button('Save'):
+    user_data = {
+        'username': st.session_state['username'],  # Replace with the actual username
+        'marks_10th': marks_10th,
+        'marks_12th': marks_12th,
+        'degree_type': degree_type,
+        'bachelors': bachelors,
+        'test_scores': test_scores,
+        'country': country,
+        'field': field
+    }
+
+    try:
+        table.put_item(Item=user_data)
+        print("Successfully inserted user input into the database")
+    except Exception as e:
+        print("Error inserting user input into the database")
+        print(e)
